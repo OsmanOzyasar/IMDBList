d90949a1e940d7c04901dc1c0ac39ccdaae30c27
osmanozysr <123673331+OsmanOzyasar@users.noreply.github.com>
2025-01-17 00:45:08 +0300

    Add project files.

---
+++ Movies.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMDBList
+{
+    public class Movies
+    {
+        public string Name { get; set; }
+
+        private double _ımdbScore;
+        public double ImdbScore 
+        {
+            get
+            {
+                return _ımdbScore;
+            }
+            set 
+            {
+                if (value < 0 || value > 10)
+                {
+                    Console.WriteLine("Puanlamayı 1 ile 10 arasında yapınız");
+                }
+                else
+                {
+                    _ımdbScore = value;
+                }
+            } 
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace IMDBList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Movies> movies = new List<Movies>();
+            
+            char userInput3;
+            do //do while döngüsüyle kullanıcıdan girmek istediği filmler ve onların puanları alınır
+            {
+                Movies movie = new Movies();
+                Console.WriteLine("Eklemek istediğiniz filmin bilgilerini giriniz: ");
+                Console.Write("Filmin adı: ");
+                string userInput1 = Console.ReadLine();
+                movie.Name = userInput1;
+
+                Console.Write("Filmin IMDB puanı: ");
+                double userInput2 = Convert.ToDouble(Console.ReadLine());
+                movie.ImdbScore = userInput2;
+
+                movies.Add(movie);
+                Console.WriteLine("Başka bir film eklemek ister misiniz? (y/n)");
+                userInput3 = Convert.ToChar(Console.ReadLine().ToLower());
+                if(userInput3 == 'n')
+                    break;
+
+            } while (userInput3 == 'y');
+
+            foreach( Movies film in movies ) // foreach döngüsüyle listeye eklenen filmler puanlarıyla birlikte ekrana yazdırılır
+            {
+                Console.WriteLine("Filmin adı: " + film.Name + " Aldığı skor: " + film.ImdbScore);
+            }
+
+            Console.WriteLine("\n4 ile 9 puan arsı filmler: ");
+            foreach (Movies film in movies)
+            {
+                if(film.ImdbScore >= 4 &&  film.ImdbScore <= 9)
+                {
+                    Console.WriteLine("Filmin adı: " + film.Name + " Aldığı skor: " + film.ImdbScore);
+                }
+            }
+
+            Console.WriteLine("\n'A' ile başlayan filmler: ");
+
+            foreach( Movies film in movies)
+            {
+                if (film.Name[0] == 'A')
+                {
+                    Console.WriteLine("Filmin adı: " + film.Name + " Aldığı skor: " + film.ImdbScore);
+                }
+            }
+        }
+    }
+}
